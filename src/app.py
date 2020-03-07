6d184a4eb942e2cd84fb13df877c327f1ea9e8af
janaosstyn <jana.osstyn@student.uantwerpen.be>
2020-03-07 09:41:12 +0100

    Add mockups for review + added boolean to simulate being logged in/not logged in

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+from forms import RegistrationForm, LoginForm, ReviewForm, Reviews
 from config import config_data
 from models import *
 
@@ -14,35 +14,35 @@ user_access = UserAccess(connection)
 @app.route("/home")
 def home():
     users = user_access.get_users()
-    return render_template('home.html', users=users)
+    return render_template('home.html', users=users, loggedIn=False)
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template('about.html', title='About', loggedIn=False)
 
 @app.route("/faq")
 def faq():
-    return render_template('faq.html', title='FAQ')
+    return render_template('faq.html', title='FAQ', loggedIn=False)
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html', title='contact')
+    return render_template('contact.html', title='contact', loggedIn=False)
 
 @app.route("/account")
 def account():
-    return render_template('account.html', title='Account')
+    return render_template('account.html', title='Account', loggedIn=True)
 
 @app.route("/edit")
 def account_edit():
-    return render_template('account_edit.html', title='Edit account info')
+    return render_template('account_edit.html', title='Edit account info', loggedIn=True)
 
 @app.route("/myrides")
 def myrides():
-    return render_template('myrides.html', title='My rides')
+    return render_template('myrides.html', title='My rides', loggedIn=True)
 
 @app.route("/user")
 def user():
-    return render_template('user.html', title='User profile')
+    return render_template('user.html', title='User profile', loggedIn=False)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -52,7 +52,7 @@ def register():
     if form.validate_on_submit():
         flash(f'Account created for {form.email.data}.', 'success')
         return redirect(url_for('home'))
-    return render_template("register.html", title="Register", form=form)
+    return render_template("register.html", title="Register", form=form, loggedIn=False)
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -66,7 +66,25 @@ def login():
             return redirect(url_for('home'))
         else:
             flash('Login failed. Please check your email and/or password.', 'danger')
-    return render_template("login.html", title="Login", form=form)
+    return render_template("login.html", title="Login", form=form, loggedIn=False)
+
+
+@app.route("/newreview", methods=['GET', 'POST'])
+def newreview():
+    form = ReviewForm()
+    return render_template('new_review.html', title='New review', form=form, loggedIn=True)
+
+
+@app.route("/reviews", methods=['GET', 'POST'])
+def reviews():
+    form = Reviews()
+    return render_template("my_reviews.html", title='My Reviews', form=form, loggedIn=True)
+
+
+@app.route("/exampleuser/reviews", methods=['GET', 'POST'])
+def exampleuser_reviews():
+    form = Reviews()
+    return render_template("my_reviews.html", title='Reviews of example user', form=form, loggedIn=True)
 
 
 if __name__ == '__main__':
--- src/forms.py
+++ src/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from jinja2 import Markup, escape
+
 
 class RegistrationForm(FlaskForm):
     first_name = StringField('First name', validators=[DataRequired()])
@@ -16,3 +18,14 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Log in')
+
+
+class ReviewForm(FlaskForm):
+    msg = StringField('', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class Reviews(FlaskForm):
+    submit_value = Markup('<b>Hello</b>')
+    filter = SubmitField('<b>Hello</b>')
+    search = StringField('')
