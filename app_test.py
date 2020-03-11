b5c35cee43d7d4c0a2b95dfd7f34b33150aecfcf
janaosstyn <jana.osstyn@student.uantwerpen.be>
2020-03-11 19:49:46 +0100

    Final test: does Travis work or not? + did some restructuring + write some tests testing if all pages can be loaded properly

--- app_test.py
+++ app_test.py
@@ -1,8 +1,85 @@
-# from src import app
-# from src.app import *
+import os
+import tempfile
+import sys
+from flask import Flask
+from src.config import config_data
+from src.models import *
+from src import app
+import unittest
 
-def func(x):
-    return x + 1
+import pytest
 
-def test_answer():
-    assert func(3) == 4
+from run import *
+
+
+# source: https://github.com/pallets/flask/blob/master/examples/tutorial/tests/conftest.py
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+        app.config['TESTING'] = True
+        self.client = app.test_client()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(app.config['DATABASE'])
+
+
+class ProperlyLoaded(FlaskTestCase):
+    def test_homepage_properly_loaded(self):
+        assert self.client.get("/").status_code == 200
+        assert self.client.get("/home").status_code == 200
+
+    def test_about_properly_loaded(self):
+        assert self.client.get("/about").status_code == 200
+
+    def test_faq_properly_loaded(self):
+        assert self.client.get("/faq").status_code == 200
+
+    def test_contact_properly_loaded(self):
+        assert self.client.get("/contact").status_code == 200
+
+    def test_account_properly_loaded(self):
+        assert self.client.get("/account").status_code == 200
+
+    def test_edit_properly_loaded(self):
+        assert self.client.get("/edit").status_code == 200
+
+    def test_myrides_properly_loaded(self):
+        assert self.client.get("/myrides").status_code == 200
+
+    def test_user_properly_loaded(self):
+        assert self.client.get("/user").status_code == 200
+
+    def test_register_properly_loaded(self):
+        assert self.client.get("/register").status_code == 200
+
+    def test_ride_info_properly_loaded(self):
+        assert self.client.get("/ride_info").status_code == 200
+
+    def test_ride_history_properly_loaded(self):
+        assert self.client.get("/ride_history").status_code == 200
+
+    def test_add_vehicle_properly_loaded(self):
+        assert self.client.get("/add_vehicle").status_code == 200
+
+    def test_login_properly_loaded(self):
+        assert self.client.get("/login").status_code == 200
+
+    def test_newreview_properly_loaded(self):
+        assert self.client.get("/newreview").status_code == 200
+
+    def test_reviews_properly_loaded(self):
+        assert self.client.get("/reviews").status_code == 200
+
+    def test_exampleuser_reviews_properly_loaded(self):
+        assert self.client.get("/exampleuser/reviews").status_code == 200
+
+    def test_findride_properly_loaded(self):
+        assert self.client.get("/findride").status_code == 200
+
+
+if __name__ == '__main__':
+    unittest.main()
