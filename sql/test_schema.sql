e3562d3f71c1c3be395d4c9cfdeeb1191657df92
janaosstyn <jana.osstyn@student.uantwerpen.be>
2020-03-12 09:08:49 +0100

    Fourteenth attempt to fix travis

---
+++ sql/test_schema.sql
@@ -0,0 +1,9 @@
+GRANT ALL PRIVILEGES ON TABLE user TO app;
+DROP TABLE IF EXISTS "user";
+CREATE TABLE "user" (
+	"first_name" VARCHAR NOT NULL,
+	"last_name" VARCHAR NOT NULL,
+	"email" VARCHAR PRIMARY KEY,
+	"password" VARCHAR NOT NULL,
+	"joined_on" TIMESTAMP
+);
