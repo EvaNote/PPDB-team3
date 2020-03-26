11df8ecb0e4a5d17c3f234ee22dff1a0c3413b46
ArnoTroch <arno.troch@student.uantwerpen.be>
2020-03-26 11:03:03 +0100

    removed inserts from schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,13 +6,3 @@ CREATE TABLE "user" (
 	"password" VARCHAR NOT NULL,
 	"joined_on" TIMESTAMP
 );
-
--- TODO create trigger for adding joined_on
-INSERT INTO "user" VALUES ('Arno', 'Troch', 'arnotroch@login.com', 'arnotroch', now());
-INSERT INTO "user" VALUES ('Eva', 'Note', 'evanote@login.com', 'evanote', now());
-INSERT INTO "user" VALUES ('Jana', 'Osstyn', 'janaosstyn@login.com', 'janaosstyn', now());
-INSERT INTO "user" VALUES ('John', 'Castillo', 'johncastillo@login.com', 'johncastillo', now());
-INSERT INTO "user" VALUES ('Robbe', 'Lauwers', 'robbelauwers@login.com', 'robbelauwers', now());
-
-
-
