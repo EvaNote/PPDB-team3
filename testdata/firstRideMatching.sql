5ba22621a9d443e30e406ab2fb18eebafc707606
janaosstyn <jana.osstyn@student.uantwerpen.be>
2020-04-21 14:02:01 +0200

    estimated_time pickup_points to query

--- testdata/firstRideMatching.sql
+++ testdata/firstRideMatching.sql
@@ -64,7 +64,7 @@ CREATE TABLE pickup_point (
     id SERIAL PRIMARY KEY,
     latitude float8 NOT NULL,
     longitude float8 NOT NULL,
-    estimated_time interval NOT NULL
+    estimated_time timestamp NOT NULL
 );
 
 /* start punt: Thomas More - Lesplaats Duffel (SNOR), coordinaten: 51.0953, 4.49607 */
@@ -79,15 +79,15 @@ values (default , '9999', 'Red', 'Toyota', 'asdf', 4, 1996, '4', 'ethanol', 1, N
 
 /* pickup 1 */
 insert into pickup_point
-values (default, 51.100562, 4.473305, '00:03');
+values (default, 51.100562, 4.473305, '2020-04-14 14:03');
 
 /* pickup 2 */
 insert into pickup_point
-values (default, 51.106853, 4.438677, '00:03');
+values (default, 51.106853, 4.438677, '2020-04-14 14:05');
 
 /* pickup 3 */
 insert into pickup_point
-values (default, 51.112011, 4.424566, '00:03');
+values (default, 51.112011, 4.424566, '2020-04-14 14:08');
 
 /* route 1:
         start punt: address 1
