751ff356425ad7e0897d5c181a731fd46593e237
RobbeLauwers <robbe.lauwers@student.uantwerpen.be>
2020-05-02 22:03:37 +0200

    nutteloze branch, maar pycharm wil niet dat ik naar master switch zodat ik dit naar master kan mergen zonder dat ik dit eerst push

--- src/dbmodels/Ride.py
+++ src/dbmodels/Ride.py
@@ -145,6 +145,7 @@ class Rides:
     def __init__(self, dbconnect):
         self.dbconnect = dbconnect
 
+    #TODO: lijkt aanhalingstekens string mee over te nemen
     def get_on(self, on, val):
         cursor = self.dbconnect.get_cursor()
         cursor.execute(
@@ -223,12 +224,18 @@ class Rides:
             results.append({"id": row[0], "email": row[1]})
         return results
 
-    def get_on_user_id(self, user_id):
-        found = self.get_on('user_id', user_id)
-        if len(found) > 0:
-            return found[0]
-        else:
-            return None
+
+    def get_on_user_id(self, val):
+        cursor = self.dbconnect.get_cursor()
+        cursor.execute(
+            "SELECT id, departure_time, arrival_time, user_id, address_1, campus, to_campus, car_id, passengers, pickup_point_1, pickup_point_2, pickup_point_3 FROM ride WHERE user_id=%s",
+            (val,))
+        rides = list()
+        for row in cursor:
+            ride = Ride(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10],
+                        row[11])
+            rides.append(ride)
+        return rides
 
     def get_on_car_id(self, car_id):
         found = self.get_on('car_id', car_id)
@@ -314,23 +321,50 @@ class Rides:
             lng_from = campus['lng']
             from_coords = 'c.latitude, c.longitude'
             campus = True
-        else:
+        elif p_from['lat']:
             lat_from = p_from['lat']
             lng_from = p_from['lng']
             from_coords = 'a.latitude, a.longitude'
             campus = True
+        else:
+            from_coords = None
         if isinstance(p_to, int):  # p_to is campus
             campus = campus_access.get_on_id(p_to).to_dict()
             lat_to = campus['lat']
             lng_to = campus['lng']
             if from_coords == 'c.latitude, c.longitude':
                 to_coords = 'a.latitude, a.longitude'
-            else:
+            elif from_coords:
                 to_coords = 'c.latitude, c.longitude'
-        else:
+            else:
+                from_coords = 'a.latitude, a.longitude'
+                lat_from = 'a.latitude'
+                lng_from = 'a.longitude'
+                to_coords = 'c.latitude, c.longitude'
+        elif p_to['lat']:
+            if not from_coords:
+                from_coords = 'a.latitude, a.longitude'
+                lat_from = 'a.latitude'
+                lng_from = 'a.longitude'
             lat_to = p_to['lat']
             lng_to = p_to['lng']
             to_coords = 'c.latitude, c.longitude'
+        else:
+            if from_coords == 'c.latitude, c.longitude':
+                to_coords = 'a.latitude, a.longitude'
+                lat_to = 'a.latitude'
+                lng_to = 'a.longitude'
+            elif from_coords:
+                to_coords = 'c.latitude, c.longitude'
+                lat_to = 'c.latitude'
+                lng_to = 'c.longitude'
+            else:
+                from_coords = 'a.latitude, a.longitude'
+                to_coords = 'c.latitude, c.longitude'
+                lat_from = 'a.latitude'
+                lng_from = 'a.longitude'
+                lat_to = 'c.latitude'
+                lng_to = 'c.longitude'
 
         cursor = self.dbconnect.get_cursor()
 
@@ -350,6 +384,8 @@ class Rides:
         else:
             p_to = str(lat_to) + ',' + str(lng_to)  # latitude, longitude
         if not p_time_option:
+            p_time_value = p_time_option
+        elif not p_datetime:
             p_time_value = p_time_option
         else:
             p_time_value = "'" + p_datetime + "'"
@@ -420,6 +456,10 @@ class Rides:
         return rides
 
     def __helper_function_dist(self, lat1, lng1, lat2, lng2):
+        if not isinstance(lat1, float) or not isinstance(lng1, float) or not isinstance(lat2, float) or not isinstance(
+                lng2, float):
+            return 0.0
+
         from src.utils import pickup_point_access
         from math import atan2, sqrt, sin, radians, cos
         return 6371000 * (2 * atan2(sqrt(sin(radians(lat2 - lat1) / 2) * sin(radians(lat2 - lat1) / 2) +
