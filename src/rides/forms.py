5ee0286c95b9a7dd315d5bb7821d4231882126c1
RobbeLauwers <robbe.lauwers@student.uantwerpen.be>
2020-05-13 01:39:33 +0200

    Niet-werkende filter rides

--- src/rides/forms.py
+++ src/rides/forms.py
@@ -1,9 +1,9 @@
-# from flask_wtf import FlaskForm
-# from flask_wtf.file import FileField, FileAllowed
-# from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, DateField, IntegerField, validators
-# from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, InputRequired
-# from src.utils import user_access
-# from flask_babel import lazy_gettext
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, DateField, IntegerField, validators
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, InputRequired
+from src.utils import user_access
+from flask_babel import lazy_gettext
 #
 # class EditRideForm(FlaskForm):
 #     first_name = StringField(lazy_gettext('First name'), validators=[DataRequired()])
@@ -16,3 +16,9 @@
 #     def validate_email(self, email):  # j
 #         if user_access.get_user_on_email(email.data):
 #             raise ValidationError(lazy_gettext('The email is already registered.'))
+
+class Filter_rides(FlaskForm):
+    #TODO: validators?
+    before = DateField(lazy_gettext("Before (yyyy-mm-dd)"))
+    after = DateField(lazy_gettext("After (yyyy-mm-dd)"))
+    submit = SubmitField(lazy_gettext('Filter'))
