b3026d182131be1773278ae8f4e99995af2c942c
RobbeLauwers <robbe.lauwers@student.uantwerpen.be>
2020-05-20 14:43:25 +0200

    myrides/joinedrides functies in hulpfuncties gesplitsts, handig voor sharedrides later

--- src/users/routes.py
+++ src/users/routes.py
@@ -19,6 +19,7 @@ from flask_babel import lazy_gettext
 from math import floor
 from ics import Calendar, Event
 from datetime import *
+from copy import deepcopy
 
 users = Blueprint('users', __name__, url_prefix='/<lang_code>')
 
@@ -320,11 +321,7 @@ def filter_rides(rides, before, after):
 def get_departure(ride):
     return ride.departure_time
 
-@users.route("/myrides", defaults={'after':None, 'before':None},methods=['GET', 'POST'])
-@users.route("/<before>myrides", defaults={'after':None},methods=['GET', 'POST'])
-@users.route("/myrides<after>", defaults={'before':None},methods=['GET', 'POST'])
-@users.route("/<before>myrides<after>", methods=['GET', 'POST'])
-def myrides(before, after):
+def myrides_help(before, after):
     before2 = None
     after2 = None
     if before is not None:
@@ -405,16 +402,30 @@ def myrides(before, after):
             return redirect(url_for('users.myrides', before=before, after=after))
         else:
             pass
+    to_ret = {}
+    to_ret["userrides"] = userrides
+    to_ret["from_locs"] = from_places
+    to_ret["to_locs"] = to_places
+    to_ret["pfps"] = pfps
+    to_ret["allids"] = allids
+    to_ret["pickuppoints"] = pickuppoints
+    to_ret["pickupbools"] = pickupbools
+    to_ret["form"] = form
+    return to_ret
 
-    return render_template('ride_history.html', title=lazy_gettext('My rides'), loggedIn=True, userrides=userrides,
-                           from_locs=from_places, to_locs=to_places, pfps=pfps, allids=allids, pickuppoints=pickuppoints,
-                           pickupbools=pickupbools, form=form, before=before, after=after)
+@users.route("/myrides", defaults={'after':None, 'before':None},methods=['GET', 'POST'])
+@users.route("/<before>myrides", defaults={'after':None},methods=['GET', 'POST'])
+@users.route("/myrides<after>", defaults={'before':None},methods=['GET', 'POST'])
+@users.route("/<before>myrides<after>", methods=['GET', 'POST'])
+def myrides(before, after):
 
-@users.route("/joinedrides", defaults={'after':None, 'before':None},methods=['GET', 'POST'])
-@users.route("/<before>joinedrides", defaults={'after':None},methods=['GET', 'POST'])
-@users.route("/joinedrides<after>", defaults={'before':None},methods=['GET', 'POST'])
-@users.route("/<before>joinedrides<after>", methods=['GET', 'POST'])
-def joinedrides(before, after):
+    res = myrides_help(before, after)
+
+    return render_template('ride_history.html', title=lazy_gettext('My rides'), loggedIn=True, userrides_m=res["userrides"],
+                           from_locs_m=res["from_locs"], to_locs_m=res["to_locs"], pfps_m=res["pfps"], allids_m=res["allids"], pickuppoints_m=res["pickuppoints"],
+                           pickupbools_m=res["pickupbools"], form=res["form"], before=before, after=after)
+
+def joinedrides_help(before, after):
     if not current_user.is_authenticated and not current_app.config['TESTING']:
         return redirect(url_for('users.login'))
     before2 = None
@@ -504,8 +515,56 @@ def joinedrides(before, after):
             return redirect(url_for('users.joinedrides', before=before, after=after))
         else:
             pass
+    to_ret = {}
+    to_ret["userrides"] = list(reversed(userrides))
+    to_ret["pickuppoints"] = list(reversed(pickuppoints))
+    to_ret["pickupbools"] = list(reversed(pickupbools))
+    to_ret["from_locs"] = list(reversed(from_places))
+    to_ret["to_locs"] = list(reversed(to_places))
+    to_ret["pfps"] = list(reversed(pfps))
+    to_ret["form"] = form
+    return to_ret
 
-    return render_template('joined_rides.html', title=lazy_gettext('Joined rides'), loggedIn=True,
+@users.route("/joinedrides", defaults={'after':None, 'before':None},methods=['GET', 'POST'])
+@users.route("/<before>joinedrides", defaults={'after':None},methods=['GET', 'POST'])
+@users.route("/joinedrides<after>", defaults={'before':None},methods=['GET', 'POST'])
+@users.route("/<before>joinedrides<after>", methods=['GET', 'POST'])
+def joinedrides(before, after):
+
+    res = joinedrides_help(before, after)
+
+    return render_template('joined_rides.html', title=lazy_gettext('View ride'), loggedIn=True,
+                           userrides_j=res["userrides"], pickuppoints_j=res["pickuppoints"], pickupbools_j=res["pickupbools"],
+                           from_locs_j=res["from_locs"], to_locs_j=res["to_locs"], pfps_j=res["pfps"], form=res["form"])
+
+
+
+@users.route("/shared_rides=<userid>")
+def shared_rides(userid):
+    if not userid.isdigit():
+        abort(404)
+    if not current_user.is_authenticated:
+        return redirect(url_for("users.user", userid=userid))
+    rides_passenger = ride_access.get_rides_from_passenger(current_user.id)
+    rides_driver =  ride_access.get_on_user_id(current_user.id)
+    if rides_driver is None:
+        rides_driver = []
+    if rides_passenger is None:
+        rides_passenger = []
+    temp = []
+    for ride in rides_driver:
+        if userid in ride_access.find_ride_passengers():
+            temp.append(ride)
+    rides_driver = deepcopy(temp)
+    temp = []
+    for ride in rides_passenger:
+        if ride.user_id == current_user.id:
+            temp.append(ride)
+    rides_passenger = deepcopy(temp)
+    rides_driver.sort(key=get_departure, reverse=True)
+    rides_passenger.sort(key=get_departure, reverse=True)
+
+    return render_template('shared_rides.html', title=lazy_gettext('Joined rides'), loggedIn=True,
                            userrides=list(reversed(userrides)), pickuppoints=list(reversed(pickuppoints)), pickupbools=list(reversed(pickupbools)),
                            from_locs=list(reversed(from_places)), to_locs=list(reversed(to_places)), pfps=list(reversed(pfps)), form=form)
 
