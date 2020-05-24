32badfe5bc38d30d241db92d675d25bf78d458c0
JohnChristianCastillo <s0175247@ad.ua.ac.be>
2020-05-24 15:10:06 +0200

    "Added google captcha"

--- run.py
+++ run.py
@@ -3,6 +3,4 @@ from src import create_app, DevelopmentConfig
 app = create_app(DevelopmentConfig)
 
 if __name__ == '__main__':
-    print(app.config['RECAPTCHA_PUBLIC_KEY'])
-    print(app.config['RECAPTCHA_PRIVATE_KEY'])
     app.run()
